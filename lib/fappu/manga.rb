ce1c92aa27e50e75cacbbb65cdf65441d659a23d
Buddy Magsipoc <keikun17@gmail.com>
2014-08-06 16:44:50 +0800

    Move parameterizing function to private。

--- lib/fappu/manga.rb
+++ lib/fappu/manga.rb
@@ -33,5 +33,32 @@ module Fappu
         self.new(manga_parameters(manga))
       end
     end
+
+    private
+
+    # Removes the 'content' suffix for manga attributes from the json response
+    def self.manga_parameters(manga)
+      params = {
+        title: manga["content_name"],
+        url: manga["content_url"],
+        description: manga["content_description"],
+        language: manga["content_language"],
+        category: manga["content_category"],
+        date: manga["content_date"],
+        filesize: manga["content_filesize"],
+        favorites: manga["content_favorites"],
+        comments: manga["content_comments"],
+        pages: manga["content_pages"],
+        poster: manga["content_poster"],
+        poster_url: manga["content_poster_url"],
+        tags: manga["content_tags"],
+        translators: manga["content_translators"],
+        series: manga["content_series"],
+        artists: manga["content_artists"],
+        images: manga["content_images"],
+        tags: manga["content_tags"]
+      }
+    end
+
   end
 end
