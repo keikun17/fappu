9f93449e33476db753f4c02ead411d1e4bc6918b
Buddy Magsipoc <keikun17@gmail.com>
2014-08-09 22:20:22 +0800

    Refactor context for manga spec. desu.

--- spec/lib/fappu/manga_spec.rb
+++ spec/lib/fappu/manga_spec.rb
@@ -3,24 +3,26 @@ require 'spec_helper'
 describe Fappu::Manga , vcr: {cassette_name: 'manga'} do
 
 
-  describe "#top_comments" do
-    context "The manga 'Right now while cleaning the pool'", vcr: { cassette_name: 'pool_cleaning_top_comments' } do
-      subject { described_class.new(url: 'https://www.fakku.net/manga/right-now-while-cleaning-the-pool') }
+  context "The manga is 'Right now while cleaning the pool'", vcr: { cassette_name: 'pool_cleaning_comments' } do
+    subject(:manga) { described_class.new(url: 'https://www.fakku.net/manga/right-now-while-cleaning-the-pool') }
+
+    describe "#top_comments" do
 
       it "has comments" do
         expect(subject.top_comments.length).not_to eq(0)
       end
 
       it "returns comment objects" do
-        expect(subject.top_comments.first).to be_a_kind_of(Fappu::Comment) 
+        expect(subject.top_comments.first).to be_a_kind_of(Fappu::Comment)
       end
 
-    end
-  end
 
-  describe "#comments" do
-    context "The manga is 'Right now while cleaning the pool' has comments", vcr: { cassette_name: 'pool_cleaning_comments' } do
-      subject { described_class.new(url: 'https://www.fakku.net/manga/right-now-while-cleaning-the-pool') }
+    end
+
+    # TODO WIP
+    describe "#comments" do
+
+      subject {manga.comments}
 
       it "is not empty"
       it "returns comment objects"
--- spec/lib/fappu/page_spec.rb
+++ spec/lib/fappu/page_spec.rb
@@ -13,6 +13,6 @@ describe Fappu::Page do
     )}
 
   end
-  
+
 end
 
